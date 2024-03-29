9ab3b74df8f52e875ca0b0432b0ae96c41899022
Alex Milanese <milaneab@dukes.jmu.edu>
2024-03-29 15:27:51 -0400

    working game page

--- whodat/homepage/views.py
+++ whodat/homepage/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from .models import Role, User, Student, Teacher, TeachingAssistant, Course, Attendance, Flashcard
+import random
 
 def homepage(request):
     return render(request, 'index.html')
 
-def game(request):
-    return render(request, 'game.html')
+def game(request, mode):
+    course = ['Student 1', 'Student 2', 'Student 3', 'Student 4', 'Student 5', 'Student 6', 'Student 7', 'Student 8']
+    
+    random_students = random.sample(course, 4)
+    answer = random.choice(random_students)
+
+    return render(request, 'game.html', {'random_students': random_students, 'answer': answer, 'course': course, 'mode': mode})
 
 def attendance(request):
-    return render(request, 'attendance.html')
+    course = Course.objects.all()
+    return render(request, 'attendance.html', {'course': course})
--- whodat/whodat/urls.py
+++ whodat/whodat/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name='homepage'),
     path('attendance/', attendance, name='attendance'),
-    path('game/', game, name='game'),
+    path('game/', game, {'mode': 'study'}, name='game'),
+    path('game/name/', game, {'mode': 'name'}, name='game_name'),
+    path('game/photo/', game, {'mode': 'photo'}, name='game_photo'),
 ]
